eb75583f63c0c12db7561ad24b27019892f5f613
Eugene Letunov <defrazo@yandex.ru>
2017-10-14 04:38:03 +0500

    Start project

---
+++ templates/include/footer.php
@@ -0,0 +1,18 @@
+	<div class="container">
+		<div class="row">
+			<div class="cols col-4"></div>
+			<div class="cols col-4">SOCIAL BUTTONS</div>
+			<div class="cols col-4"></div>
+		</div>
+
+		<div class="row">
+			<div class="cols col-2"></div>
+			<div class="cols col-2">Button</div>
+			<div class="cols col-2">Button</div>
+			<div class="cols col-2">Button</div>
+			<div class="cols col-2">Button</div>
+			<div class="cols col-2"></div>
+		</div>
+	</div>
+
+</html>
---
+++ templates/main.php
@@ -0,0 +1,34 @@
+<?php include "../templates/include/header.php" ?>
+
+<body>
+	<div class="container">
+		<div class="row" id="row1">
+			<div class="cols col-5"><b>Монетка</b></div>
+			<div class="cols col-3">+Пригласить</div>
+			<div class="cols col-4">Кнопки управления</div>
+		</div>
+
+		<div class="row">
+			<div class="cols col-2">Ава</div>
+			<div class="cols col-2">Счет</div>
+			<div class="cols col-2">+Купить</div>
+			<div class="cols col-3">Реклама1</div>
+			<div class="cols col-3">Реклама2</div>
+		</div>
+
+		<div class="row">
+			<div class="cols col-2">Новости</div>
+			<div class="cols col-2">Стол 1₽</div>
+			<div class="cols col-2">Стол 5₽</div>
+			<div class="cols col-2">Стол 10₽</div>
+			<div class="cols col-2">Стол 50₽</div>
+			<div class="cols col-2">Стол 100₽</div>
+		</div>
+		<div class="row">
+			<div class="cols col-2"></div>
+			<div class="cols col-8">НОВОСТНОЙ БЛОК</div>
+			<div class="cols col-2">Даты</div>
+		</div>
+	</div>
+</body>
+<?php include "../templates/include/footer.php" ?>
